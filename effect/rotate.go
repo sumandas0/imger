d2c01936ec3e1d0bb503e373446514d09fe68a9d
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 05:07:09 +0000

    Rotate transformer added

--- effect/rotate.go
+++ effect/rotate.go
@@ -3,6 +3,9 @@ package effect
 import (
 	"context"
 	"image"
+	"image/color"
+
+	"github.com/disintegration/imaging"
 )
 
 type rotate struct {
@@ -10,6 +13,18 @@ type rotate struct {
 }
 
 func (r *rotate) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
+	angle, err := floatBinder("angle", params)
 
+	if err != nil {
+		return nil, err
+	}
+
+	bgColor, err := colorBinder("bgcolor", params)
+
+	if err != nil {
+		bgColor = color.Transparent
+	}
+
+	img = imaging.Rotate(img, angle, bgColor)
 	return img, ctx.Err()
 }
