49fbfef11e3f537d7db4c6afa567bf32153d73c2
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 07:02:49 +0000

    NewContrast object

--- effect/contrast.go
+++ effect/contrast.go
@@ -4,11 +4,29 @@ import (
 	"context"
 	"image"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type contrast struct {
 	effect
+}
+
+func NewContrast() imger.Effect {
+	return &contrast{
+		effect: effect{
+			id:          "contrast",
+			description: "Contrast - Change the image contrast",
+			parameters: imger.Parameters{
+				"percentage": imger.Parameter{
+					Description: "Percentage of the contrast.",
+					Required:    true,
+					Example:     10,
+					Type:        "float",
+				},
+			},
+		},
+	}
 }
 
 func (r *contrast) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
