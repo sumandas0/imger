cbb3c112dde5308de30214c761c345a6283fad44
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 06:50:53 +0000

    NewBrightness added

--- effect/brightness.go
+++ effect/brightness.go
@@ -4,11 +4,30 @@ import (
 	"context"
 	"image"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type brightness struct {
 	effect
+}
+
+// NewBrightness creates an Effect changes the image brightnness
+func NewBrightness() imger.Effect {
+	return &brightness{
+		effect: effect{
+			id:          "brightness",
+			description: "Brightness - Change the image brightness",
+			parameters: imger.Parameters{
+				"percentage": imger.Parameter{
+					Description: "Percentage of the brightness.",
+					Required:    true,
+					Example:     0.5,
+					Type:        "float",
+				},
+			},
+		},
+	}
 }
 
 func (r *brightness) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
