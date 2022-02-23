86755b1cfc696cb6b11ca9d0058724c1c4be3454
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 04:44:03 +0000

    Effect Repo added

--- imger/effect.go
+++ imger/effect.go
@@ -32,3 +32,11 @@ type Effect interface {
 	// Transform applies the specific transformation to the given image
 	Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error)
 }
+
+// EffectRepository to store effects
+type EffectRepository interface {
+	// GetEffects return all available effects
+	GetEffects() ([]Effect, error)
+	// GetEffect returns an effect by the given id
+	GetEffect(id string) (Effect, error)
+}
