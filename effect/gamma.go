135f5892849e247ca07615c3d28b49a05cd13634
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 06:54:01 +0000

    Gamma New Object added

--- effect/gamma.go
+++ effect/gamma.go
@@ -4,11 +4,29 @@ import (
 	"context"
 	"image"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type gamma struct {
 	effect
+}
+
+func NewGamma() imger.Effect {
+	return &gamma{
+		effect: effect{
+			id:          "gamma",
+			description: "Gamma - Change the image gamma",
+			parameters: imger.Parameters{
+				"gamma": imger.Parameter{
+					Description: "Percentage of the gamma correction.",
+					Required:    true,
+					Example:     0.75,
+					Type:        "float",
+				},
+			},
+		},
+	}
 }
 
 func (r *gamma) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
