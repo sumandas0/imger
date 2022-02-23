41452bb56cc1a759137aaa982a8d817783013d6e
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 06:38:41 +0000

    newResizer added

--- effect/resize.go
+++ effect/resize.go
@@ -4,11 +4,43 @@ import (
 	"context"
 	"image"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type resize struct {
 	effect
+}
+
+func NewResize() imger.Effect {
+	return &resize{
+		effect: effect{
+			id:          "resize",
+			description: "Resize - resizes an image",
+			parameters: imger.Parameters{
+				"width": imger.Parameter{
+					Description: "Width in px",
+					Required:    true,
+					Example:     500,
+					Type:        "integer",
+				},
+				"height": imger.Parameter{
+					Description: "Height in px",
+					Required:    true,
+					Example:     350,
+					Type:        "integer",
+				},
+				"filter": imger.Parameter{
+					Description: "Resample filter",
+					Required:    false,
+					Example:     "linear",
+					Type:        "string",
+					Default:     "linear",
+					Values:      filtersList,
+				},
+			},
+		},
+	}
 }
 
 func (r *resize) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
