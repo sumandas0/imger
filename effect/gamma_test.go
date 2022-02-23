d299664189ec8d5f262f0884e8111676a4b90949
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 06:54:54 +0000

    Gamma Feature added and Tested

---
+++ effect/gamma_test.go
@@ -0,0 +1,40 @@
+package effect
+
+import (
+	"context"
+	"image"
+	"testing"
+
+	"github.com/RexterR/imger/errors"
+)
+
+func TestGammaTransform(t *testing.T) {
+	tt := []struct {
+		name   string
+		params map[string]interface{}
+		err    errors.Type
+	}{
+		{
+			name:   "transform successfully",
+			params: map[string]interface{}{"gamma": 0.9}},
+		{
+			name:   "missing gamma",
+			params: map[string]interface{}{}, err: errors.Validation,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			img := image.NewRGBA(image.Rect(0, 0, 100, 50))
+			gamma := NewGamma()
+
+			_, err := gamma.Transform(context.Background(), img, tc.params)
+
+			if tc.err != "" {
+				if err == nil || !errors.Is(tc.err, err) {
+					t.Error("Expected validation error", err)
+				}
+			}
+		})
+	}
+}
