2d0db48be1a1ab16e92c0ab2ed4fb3a4a12f282d
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 05:24:07 +0000

    New Rotator created

--- effect/rotate.go
+++ effect/rotate.go
@@ -5,11 +5,37 @@ import (
 	"image"
 	"image/color"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type rotate struct {
 	effect
+}
+
+func NewRotate() imger.Effect {
+	return &rotate{
+		effect: effect{
+			id:          "rotate",
+			description: "Rotate - rotates an image",
+			parameters: imger.Parameters{
+				"angle": imger.Parameter{
+					Description: "Rotation angle in degreesº",
+					Required:    true,
+					Example:     -90,
+					Type:        "integer",
+				},
+				"bgcolor": imger.Parameter{
+					Description: "Color of uncovered zones",
+					Required:    false,
+					Example:     "black",
+					Type:        "string",
+					Default:     "transparent",
+					Values:      colorsList,
+				},
+			},
+		},
+	}
 }
 
 func (r *rotate) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
