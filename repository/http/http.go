6fe19e3ebbd360d6292938ab08a8a9c5272f6862
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 07:22:54 +0000

    Get Image by HTTP

---
+++ repository/http/http.go
@@ -0,0 +1,70 @@
+package http
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"image"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/RexterR/imger/errors"
+	"github.com/RexterR/imger/imger"
+)
+
+const maxImageSize = 1024 * 1024 * 5
+
+type httpdownloader struct {
+	client *http.Client
+}
+
+// NewImageRepository creates a Downloader that get an image over the HTTP protocol.
+func NewImageRepository() imger.ImageRepository {
+	return &httpdownloader{
+		client: &http.Client{
+			Timeout: time.Second * 30,
+		},
+	}
+}
+
+func (d *httpdownloader) Get(ctx context.Context, path string) (image.Image, string, error) {
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+
+	if err != nil {
+		return nil, "", errors.EInternal("Error trying to create http request", err)
+	}
+
+	req = req.WithContext(ctx)
+
+	response, err := d.client.Do(req)
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusNotFound {
+		return nil, "", errors.ENotExists(fmt.Sprintf("Image %s not found", path), nil)
+	}
+
+	imgBytes := &bytes.Buffer{}
+
+	if _, err := io.CopyN(imgBytes, response.Body, maxImageSize); err != io.EOF {
+
+		if ctx.Err() != nil {
+			return nil, "", ctx.Err()
+		}
+
+		return nil, "", errors.EValidation(fmt.Sprintf("Image size is bigger than: %d", maxImageSize), err)
+	}
+
+	img, imgType, err := image.Decode(imgBytes)
+
+	if err != nil {
+		return nil, "", errors.EInternal("Error decoding image", err)
+	}
+
+	return img, imgType, ctx.Err()
+}
