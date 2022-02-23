d544728179006d021cb431929dc1c37a62fee779
Suman Das <59254445+RexterR@users.noreply.github.com>
2022-02-23 06:56:53 +0000

    Crop New Objecter added

--- effect/crop.go
+++ effect/crop.go
@@ -4,11 +4,30 @@ import (
 	"context"
 	"image"
 
+	"github.com/RexterR/imger/imger"
 	"github.com/disintegration/imaging"
 )
 
 type crop struct {
 	effect
+}
+
+// NewCrop creates an Effect that crops the image
+func NewCrop() imger.Effect {
+	return &crop{
+		effect: effect{
+			id:          "crop",
+			description: "Crop - Crops image",
+			parameters: imger.Parameters{
+				"rectangle": imger.Parameter{
+					Description: "Region to crop (x0,y0,x1,y1)",
+					Required:    true,
+					Example:     "[0,0,100,200]",
+					Type:        "array[int]",
+				},
+			},
+		},
+	}
 }
 
 func (o *crop) Transform(ctx context.Context, img image.Image, params map[string]interface{}) (image.Image, error) {
